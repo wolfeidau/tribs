ce310defc0608eb34303d2f7b0e9606a5e212508
Mark Wolfe <mark@wolfe.id.au>
2012-05-28 12:56:43 +1000

    Better test coverage for the issue log service.

--- src/test/java/au/id/wolfe/tribs/service/IssueLogServiceImplTest.java
+++ src/test/java/au/id/wolfe/tribs/service/IssueLogServiceImplTest.java
@@ -4,15 +4,18 @@ import au.id.wolfe.tribs.data.IssueLogEntry;
 import au.id.wolfe.tribs.data.IssueLogReport;
 import au.id.wolfe.tribs.repository.IssueRepository;
 import au.id.wolfe.tribs.service.impl.IssueLogServiceImpl;
+import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.issue.changehistory.ChangeHistoryItem;
 import com.atlassian.jira.issue.changehistory.ChangeHistoryManager;
 import com.atlassian.jira.issue.issuetype.IssueType;
 import com.atlassian.jira.issue.priority.Priority;
 import com.atlassian.jira.project.Project;
 import com.atlassian.jira.project.ProjectManager;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang.time.DateUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,10 +27,12 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -103,6 +108,12 @@ public class IssueLogServiceImplTest {
             when(priority.getName()).thenReturn("Major");
             when(issue.getPriorityObject()).thenReturn(priority);
 
+            User assignedUser = newUser("Mark Wolfe", "mark.wolfe@hsd.com.au");
+            User reportingUser = newUser("Peter Sawyer", "peter@hsd.com.au");
+
+            when(issue.getAssigneeUser()).thenReturn(assignedUser);
+            when(issue.getReporterUser()).thenReturn(reportingUser);
+
             when(issueManager.getIssueObject((long) i)).thenReturn(issue);
 
             when(issue.getKey()).thenReturn(String.format("TEST-%d", i));
@@ -110,6 +121,11 @@ public class IssueLogServiceImplTest {
             when(issue.getDescription()).thenReturn(String.format("Some really really really big issue with an i of %d", i));
             when(issue.getCreated()).thenReturn(new Timestamp(System.currentTimeMillis()));
 
+            List<ChangeHistoryItem> changeHistoryItems = Lists.newLinkedList();
+
+            changeHistoryItems.add(newChangeHistoryItem("status", "1", "Open", "3", "Resolved"));
+
+            when(changeHistoryManager.getAllChangeItems(any(Issue.class))).thenReturn(changeHistoryItems);
 
             issueIdList.add((long) i);
 
@@ -119,5 +135,29 @@ public class IssueLogServiceImplTest {
         return issueIdList;
     }
 
+    private User newUser(String displayName, String emailAddress) {
+        User user = mock(User.class);
+        when(user.getDisplayName()).thenReturn(displayName);
+        when(user.getEmailAddress()).thenReturn(emailAddress);
+        return user;
+    }
 
+
+    private ChangeHistoryItem newChangeHistoryItem(String field, String fromKey, String fromDescription, String toKey, String toDescription){
+
+        ChangeHistoryItem changeHistoryItem = mock(ChangeHistoryItem.class);
+
+        when(changeHistoryItem.getField()).thenReturn(field);
+
+        Map<String, String> tos = Maps.newHashMap();
+        tos.put(toKey, toDescription);
+
+        Map<String, String> froms = Maps.newHashMap();
+        froms.put(fromKey, fromDescription);
+
+        when(changeHistoryItem.getTos()).thenReturn(tos);
+        when(changeHistoryItem.getFroms()).thenReturn(froms);
+
+        return changeHistoryItem;
+    }
 }
